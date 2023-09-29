5bacb3e8f5f0ee82137c615ff413c27aa5e5d7eb
javiardid <javier.ardiddiaz@cieep.com>
2023-09-29 11:25:46 +0200

    Initial commit

---
+++ app/src/main/java/javi/ardid/ejercicio2/actividades/CrearMotoActivity.java
@@ -0,0 +1,16 @@
+package javi.ardid.ejercicio2.actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import javi.ardid.ejercicio2.R;
+
+public class CrearMotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_moto);
+    }
+}
---
+++ app/src/main/java/javi/ardid/ejercicio2/modelos/Bici.java
@@ -0,0 +1,41 @@
+package javi.ardid.ejercicio2.modelos;
+
+import java.io.Serializable;
+
+public class Bici implements Serializable {
+    private String marca;
+    private int pulgadas;
+
+
+    public Bici() {
+    }
+
+    public Bici(String marca, int pulgadas) {
+        this.marca = marca;
+        this.pulgadas = pulgadas;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getPulgadas() {
+        return pulgadas;
+    }
+
+    public void setPulgadas(int pulgadas) {
+        this.pulgadas = pulgadas;
+    }
+
+    @Override
+    public String toString() {
+        return "Bici{" +
+                "marca='" + marca + '\'' +
+                ", pulgadas=" + pulgadas +
+                '}';
+    }
+}
